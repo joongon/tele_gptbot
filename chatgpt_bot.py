64d9da1237844a55254fc57d8e0c2d52eb6b897e
joongonrigid@gmail.com <joongonrigid@gmail.com>
2023-04-19 22:24:34 +0900

    trigger toggle addition

--- chatgpt_bot.py
+++ chatgpt_bot.py
@@ -14,13 +14,17 @@ from transformers import GPT2Tokenizer
 TOKEN = "TELEGRAM BOT TOKEN HERE" #Telegram bot의 TOKEN 입력, 보안상 환경변수로 설정을 해도 된다.
 approved_id = []#봇 사용이 허가된 대화방 id 리스트, 반드시 정수형을 입력, "" or '' 삽입금지
 #ex) approved_id = [23456, -123445, 112333] --> 23456 or -123445, 112222 3개의 채팅방에 대해 본 chatbot 사용허가
+#대화방 chat_id 확인 주소 : https://api.telegram.org/bot토큰/getUpdates
 bot_name = "BOT NAME" #Telegram bot name을 입력한다.
 
 #========== Telegram bot Trigger 설정 ===================================
+enableTrigger = True #Trigger 없이 모든 메세지를 트리거로 할 경우 False
+#False일 경우 아래 Trigger 설정은 적용 안되며, GPT3.5 Turbo만 사용, 기타 DALL.E / 
+
 CHAT = "/t" #GPT3.5 Turbo 엔진을 사용한 채팅 트리거 ex) /t
-CODE = "/c" #code-davinci-002 엔진을 사용한 코딩 트리거 ex) /c
-DRAW = "/p" #Image 생성기 트리거 ex) /p
-TEXT = "/txt" #text-davinci-003 엔진을 활용한 문장완성 트리거 ex) /txt
+# CODE = "/c" #code-davinci-002 엔진을 사용한 코딩 트리거 ex) /c deprecated now
+DRAW = "/p" #DALL.E 엔진을 이용 Image 생성기 트리거 ex) /p 
+TEXT = "/txt4" #gpt-4-32k 엔진을 활용한 문장완성 트리거 ex) /txt
 #======================================================================
 
 #===========OPENAI API Info =========================================
@@ -28,7 +32,7 @@ API_KEY = "" #OPENAI API KEY를 입력한다.
 openai.api_key = API_KEY
 #=====================================================================
 
-triggers = [CHAT, CODE, DRAW, TEXT]
+triggers = [CHAT, DRAW, TEXT]
 messages = [{"role": "system", "content": "You are a helpful assistant."}]
 
 def tokenizer(sequence):
@@ -135,17 +139,17 @@ def text_completer(prompt):
     text = response['choices'][0]['text']
     return text
 
-def code_maker(prompt):
-    response = openai.Completion.create(model="code-davinci-002",\
-    prompt=prompt,\
-    temperature=0,\
-    max_tokens=4000,\
-    top_p=1.0,\
-    frequency_penalty=1.0,\
-    presence_penalty=0.0,\
-    stop=["\"\"\""])
-    code = response['choices'][0]['text']
-    return code
+# def code_maker(prompt):
+#     response = openai.Completion.create(model="code-davinci-002",\
+#     prompt=prompt,\
+#     temperature=0,\
+#     max_tokens=4000,\
+#     top_p=1.0,\
+#     frequency_penalty=1.0,\
+#     presence_penalty=0.0,\
+#     stop=["\"\"\""])
+#     code = response['choices'][0]['text']
+#     return code
 
 def trash_remover(target):
     p = re.compile('[+]')
@@ -163,51 +167,54 @@ def echo(update, context):
     print("Chat_id = ", user_id)
     user_text = update.message.text
     user_text = user_text.split()
+    
+    
 
     if user_id in approved_id:
-        if user_text[0] == triggers[2]:
+        
+        if enableTrigger == True:
+            
+            if user_text[0] == triggers[1]:
+                final_text = ""
+                
+                for i in range(1, len(user_text)):
+                    final_text += user_text[i] + " "
+                    print(final_text)
+                path = img_maker(final_text)
+                context.bot.sendPhoto(chat_id=user_id, photo=open(path, 'rb'))
+                subprocess.call(["rm", path])
+    #triggers = [CHAT, DRAW, TEXT]
+                
+            elif user_text[0] == triggers[0]:
+                final_text = ""
+                
+                for i in range(1, len(user_text)):
+                    final_text += user_text[i] + " "
+                    print(final_text)
+                text = chat_completion(final_text)
+                context.bot.send_message(chat_id=user_id, text=text, parse_mode='Markdown')
+
+            elif user_text[0] == triggers[2]:
+                final_text = ""
+                
+                for i in range(1, len(user_text)):
+                    final_text += user_text[i] + " "
+                    print(final_text)
+                try:
+                    text = text_completer2(final_text)
+                except:
+                    text = "현재 ChatGPT4 API Beta 모델 엔진 사용신청중이며, Capacity 확부 후 사용 가능해 집니다. 시간이 다소 걸릴 것 같습니다."
+                context.bot.send_message(chat_id=user_id, text=text)  
+        elif enableTrigger == False:
             final_text = ""
-            
-            for i in range(1, len(user_text)):
-                final_text += user_text[i] + " "
-                print(final_text)
-            path = img_maker(final_text)
-            context.bot.sendPhoto(chat_id=user_id, photo=open(path, 'rb'))
-            subprocess.call(["rm", path])
-#triggers = [CHAT, CODE, DRAW, TEXT]
-        elif user_text[0] == triggers[1]:
-            final_text = ""
-            
-            for i in range(1, len(user_text)):
-                final_text += user_text[i] + " "
-                print(final_text)
-            try:
-                text_temp = trash_remover(code_maker(final_text))
-                text = text_temp[:4095]
-            except Exception as e:
-                text = e
-            context.bot.send_message(chat_id=user_id, text=text)
-            
-        elif user_text[0] == triggers[0]:
-            final_text = ""
-            
             for i in range(1, len(user_text)):
                 final_text += user_text[i] + " "
                 print(final_text)
             text = chat_completion(final_text)
             context.bot.send_message(chat_id=user_id, text=text, parse_mode='Markdown')
+        else:
+            pass
 
-        elif user_text[0] == '/txt4':
-            final_text = ""
-            
-            for i in range(1, len(user_text)):
-                final_text += user_text[i] + " "
-                print(final_text)
-            try:
-                text = text_completer2(final_text)
-            except:
-                text = "현재 ChatGPT4 API Beta 모델 엔진 사용신청중이며, Capacity 확부 후 사용 가능해 집니다. 시간이 다소 걸릴 것 같습니다."
-            context.bot.send_message(chat_id=user_id, text=text)   
     else:
         text = f"You or this group is not allowed to use the bot({bot_name}). I warn you no to use this bot anymore."
         context.bot.send_message(chat_id=user_id, text=text)
